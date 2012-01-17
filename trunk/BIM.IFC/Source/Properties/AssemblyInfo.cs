688164fa8d43126eddeebbfe3706ece6a485ea54
tauyoung <tauyoung@8a78862c-6a85-4826-aa44-55e33aeebcc6>
2012-01-17 07:35:06 +0000

    Update copyright

--- trunk/BIM.IFC/Source/Properties/AssemblyInfo.cs
+++ trunk/BIM.IFC/Source/Properties/AssemblyInfo.cs
@@ -1,5 +1,17 @@
 using System.Reflection;
 
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("IFC Exporter for Revit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Autodesk, Inc.")]
+[assembly: AssemblyProduct("IFC Exporter for Revit")]
+[assembly: AssemblyCopyright("© 2011 Autodesk, Inc. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
 [assembly: AssemblyVersion("1.0.4.0")]
 [assembly: AssemblyFileVersion("1.0.4.0")]
 
