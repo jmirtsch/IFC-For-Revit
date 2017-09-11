87dce328f64dcc5c4f2e334460437fcd5de38d83
unknown <jonm@geometrygym.com>
2017-09-12 07:02:54 +1000

    browser ui

---
+++ Source/Revit.IFC.Export/Browser.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit.PropertyGrid;
+
+using Autodesk.Revit.UI;
+
+namespace Revit.IFC.Export
+{
+   /// <summary>
+   /// Interaction logic for Browser.xaml
+   /// </summary>
+   public partial class Browser : Page, IDockablePaneProvider
+   {
+      public Browser()
+      {
+         InitializeComponent();
+      }
+
+      public void SetupDockablePane(DockablePaneProviderData data)
+      {
+         data.FrameworkElement = this as FrameworkElement;
+         data.InitialState = new DockablePaneState();
+         data.InitialState.DockPosition = DockPosition.Tabbed;
+         //DockablePaneId targetPane;
+         //if (m_targetGuid == Guid.Empty)
+         //    targetPane = null;
+         //else targetPane = new DockablePaneId(m_targetGuid);
+         //if (m_position == DockPosition.Tabbed)
+         data.InitialState.TabBehind = Autodesk.Revit.UI.DockablePanes.BuiltInDockablePanes.ProjectBrowser;
+         //if (m_position == DockPosition.Floating)
+         //{
+         //data.InitialState.SetFloatingRectangle(new Autodesk.Revit.UI.Rectangle(10, 710, 10, 710));
+         //data.InitialState.DockPosition = DockPosition.Tabbed;
+         //}
+         //Log.Message("***Intial docking parameters***");
+         //Log.Message(APIUtility.GetDockStateSummary(data.InitialState));
+      }
+   }
+}
---
+++ Source/Revit.IFC.Export/BrowserCommandApplication.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Threading;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Events;
+
+namespace Revit.IFC.Export.UI
+{
+   public class PropertyBrowser : IExternalApplication
+   {
+      private RequestHandler mHandler;
+      Browser m_MyDockableWindow = null;
+      private ExternalEvent mExEvent;
+
+      public Result OnStartup(UIControlledApplication a)
+      {
+         DockablePaneProviderData data = new DockablePaneProviderData();
+         m_MyDockableWindow = new Browser();
+         data.FrameworkElement = m_MyDockableWindow as System.Windows.FrameworkElement;
+         data.InitialState = new DockablePaneState();
+         data.InitialState.DockPosition = DockPosition.Tabbed;
+
+         DockablePaneId dpid = new DockablePaneId(new Guid("{C7C70722-1B9B-4454-A054-DFD142F23580}"));
+         a.RegisterDockablePane(dpid, "IFC Properties", m_MyDockableWindow as IDockablePaneProvider);
+
+        foreach (Autodesk.Windows.RibbonTab tab in Autodesk.Windows.ComponentManager.Ribbon.Tabs)
+         {
+            if (tab.Id == "Modify")
+            {
+               tab.PropertyChanged += PanelEvent;
+               break;
+            }
+         }
+         //mHandler = new RequestHandler(this);
+         mExEvent = ExternalEvent.Create(mHandler);
+         return Result.Succeeded;
+      }
+
+      public Result OnShutdown(UIControlledApplication a)
+      {
+         return Result.Succeeded;
+      }
+
+      void PanelEvent(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+      {
+         if (e.PropertyName == "Title")
+         {
+
+         }
+      }
+
+      private void MakeRequest(string json)
+      {
+         mExEvent.Raise();
+      }
+   }
+
+   public class RequestHandler : IExternalEventHandler
+   {
+      private Request mRequest = new Request();
+      public Request Request { get { return mRequest; } }
+
+      //private 
+      public RequestHandler(Form form)
+      {
+         //mForm = form;
+      }
+
+      public String GetName() { return "BimForce UOB"; }
+      public void Execute(UIApplication uiapp)
+      {
+         try
+         {
+            ICollection<ElementId> elementIds = uiapp.ActiveUIDocument.Selection.GetElementIds(); 
+
+         }
+         finally
+         {
+         }
+
+         return;
+      }
+   }
+
+   public class Request
+   {
+      //public string Take()
+      //{
+      //   string str = "";
+      //   return Interlocked.Exchange(ref mJson, str);
+      //}
+      //public void Make(string json)
+      //{
+      //   Interlocked.Exchange(ref mJson, json);
+      //}
+   }
+   [Transaction(TransactionMode.Manual)]
+   public class RegisterDockableWindow : IExternalCommand
+   {
+      public Result Execute(
+     ExternalCommandData commandData,
+     ref string message,
+     ElementSet elements)
+      {
+         
+         return Result.Succeeded;
+      }
+   }
+}
+
